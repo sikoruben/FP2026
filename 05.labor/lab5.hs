801db55c697fa63592ff404d7adeff4d5c7beded
sikoruben <sikoruben@gmail.com>
2026-04-01 12:45:31 +0300

    lab6 potlas

--- 05.labor/lab5.hs
+++ 05.labor/lab5.hs
@@ -1,3 +1,6 @@
+import Data.List
+import Distribution.Simple.Setup (trueArg)
+
 -- # 5. labor
 
 -- I. Írjuk meg a beépített splitAt, notElem, concat, repeat, replicate, cycle, iterate, any, all függvényeket.
@@ -31,10 +34,118 @@ myAll p (x : xs) = p x && myAll p xs
 
 -- - implementálja a length, sum, elem, reverse, product, maximum, insert-sort, ++, map, filter függvényeket,
 
+myLengthL ls = foldl op 0 ls
+  where
+    op res k = res + 1
+
+myLengthR ls = foldr op 0 ls
+  where
+    op k res = res + 1
+
+mySumL ls = foldl op 0 ls
+  where
+    op res k = res + k
+
+mySumR ls = foldr op 0 ls
+  where
+    op k res = res + k
+
+myElemL c ls = foldl (op c) False ls
+  where
+    op c res k
+      | c == k = True
+      | otherwise = res
+
+myElemR c ls = foldr (op c) False ls
+  where
+    op c k res = (c == k) || res
+
+myReverseL ls = foldl op [] ls
+  where
+    op res k = k : res
+
+myReverseR ls = foldr op [] ls
+  where
+    -- op k res = res ++ [k]
+    op k res = res <> [k]
+
+myProductL ls = foldl op 1 ls
+  where
+    op res k = res * k
+
+myProductR ls = foldl op 1 ls
+  where
+    op k res = res * k
+
+myMaximumL ls = foldl op (head ls) ls
+  where
+    op res k
+      | res > k = res
+      | otherwise = k
+
+myMaximumL1 ls = foldl1 op ls -- kezdoertek a lista elso eleme lesz automatikusan
+  where
+    op res k
+      | res > k = res
+      | otherwise = k
+
+-- erre a muveletre nem alkalmas a fold
+myIns x ls = foldr (op x) [] ls
+  where
+    op x k res
+      | x > k = k : res
+      | otherwise = x : k : res
+
+ins :: (Ord a) => a -> [a] -> [a]
+ins x [] = [x]
+ins x (k : ve)
+  | x > k = k : ins x ve
+  | otherwise = x : k : ve
+
+myAppend ls1 ls2 = foldr op ls2 ls1
+  where
+    op k res = k : res
+
+myMap fg ls = foldr (op fg) [] ls
+  where
+    op fg k res = fg k : res
+
+myFilter fg ls = foldr (op fg) [] ls
+  where
+    op fg k res
+      | fg k == True = k : res
+      | otherwise = res
+
 -- - meghatározza egy lista pozitív elemeinek összegét,
+
+mySumPos ls = foldl op 0 ls
+  where
+    op res k
+      | k > 0 = res + k
+      | otherwise = res
+
 -- - egy lista páros elemeinek szorzatát,
+
+myProduct ls = foldl op 1 ls
+  where
+    op res k
+      -- \| mod k 2 == 0 = res * k
+      | odd k = res * k
+      | otherwise = res
+
 -- - n-ig a négyzetszámokat.
+
+negyzetszam n = foldr op [] [1 .. n]
+  where
+    op k res = k * k : res
+
 -- - meghatározza a $$P(x) = a_0 + a_1 x + a_2 x^2 + \ldots + a_n x^n$$ polinom adott $x_0$ értékre való behelyettesítési értékét: $$a_0 + x_0(a_1 + x_0(a_2 + x_0(a_3 + \ldots + x_0(a_{n-1}+ x_0 \cdot a_n))))$$
+
+polinom x0 ls = foldr (op x0) 0 ls
+  where
+    op x0 k res = k + x0 * res
+
+mySum2 a b = foldr (+) 0 [a + 1 .. b - 1]
 
 -- III.
 
